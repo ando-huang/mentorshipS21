2dee666f53ce64b83a9ff2a7925f3b73ac9f43ab
ando-huang <ahuang09182000@gmail.com>
2021-04-10 08:26:39 -0400

    demo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,16 @@
 import Header from './Components/Header'
+import Experience from './Components/Experience'
 import './App.css';
 
 function App() {
   return (
     <div>
       <div 
-      className="header"
+        className="header"
       >
         <Header/>
       </div>
+      <Experience Company = {'Yahoo'}/>
       Body
     </div>
   );
---
+++ my-app/src/Components/Experience.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+
+const Experience = (props) => {
+    return(
+        <div>
+            {props.Company}, 2020 - 2021
+        </div>
+    )
+}
+
+export default Experience;
